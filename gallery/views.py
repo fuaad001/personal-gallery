fa12ef5c34d5873266facddd101878a991c0138c
fuaad001 <husseinfuaad@hotmail.com>
2019-05-12 15:01:16 +0300

    Create index view

--- gallery/views.py
+++ gallery/views.py
@@ -8,24 +8,24 @@ def index(request):
 
     return render(request, 'index.html', {"images": images})
 
-def search_page(request):
-
-    if 'search' in request.GET and request.GET["search"]:
-        search_term = request.GET.get("search")
-        images = Image.search_image(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'search.html', {"message": message, "images": images})
-
-    else:
-        message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
-
-def sortby_locations(request):
-    images = Image.filter_by_location()
-
-    return render(request, 'location.html', {"images":images})
-
-def single_image(request, image_id):
-    image = Image.get_image_by_id(image_id)
-    return render(request, 'single_image.html', {"image":image})
+# def search_page(request):
+#
+#     if 'search' in request.GET and request.GET["search"]:
+#         search_term = request.GET.get("search")
+#         images = Image.search_image(search_term)
+#         message = f"{search_term}"
+#
+#         return render(request, 'search.html', {"message": message, "images": images})
+#
+#     else:
+#         message = "You haven't searched for any term"
+#         return render(request, 'search.html',{"message":message})
+#
+# def sortby_locations(request):
+#     images = Image.filter_by_location()
+#
+#     return render(request, 'location.html', {"images":images})
+#
+# def single_image(request, image_id):
+#     image = Image.get_image_by_id(image_id)
+#     return render(request, 'single_image.html', {"image":image})
