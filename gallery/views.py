bc1fe085ea816e40966acf343209a48175b51266
fuaad001 <husseinfuaad@hotmail.com>
2019-05-12 15:02:01 +0300

    Create search_page view

--- gallery/views.py
+++ gallery/views.py
@@ -8,19 +8,19 @@ def index(request):
 
     return render(request, 'index.html', {"images": images})
 
-# def search_page(request):
-#
-#     if 'search' in request.GET and request.GET["search"]:
-#         search_term = request.GET.get("search")
-#         images = Image.search_image(search_term)
-#         message = f"{search_term}"
-#
-#         return render(request, 'search.html', {"message": message, "images": images})
-#
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'search.html',{"message":message})
-#
+def search_page(request):
+
+    if 'search' in request.GET and request.GET["search"]:
+        search_term = request.GET.get("search")
+        images = Image.search_image(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message": message, "images": images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
 # def sortby_locations(request):
 #     images = Image.filter_by_location()
 #
