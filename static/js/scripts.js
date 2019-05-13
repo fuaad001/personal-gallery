aee35c5e99e2cfb968b25dbf99e4cb5ccfd2d34a
fuaad001 <husseinfuaad@hotmail.com>
2019-05-13 11:48:31 +0300

    Deployment to heroku

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -18,3 +18,17 @@ $(document).ready(function(){
     $('#modal-details').css('display', 'none')
   })
 })
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
