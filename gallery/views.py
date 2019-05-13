b01376936745a4c9facbebaf27cb4cb81ecc3723
fuaad001 <husseinfuaad@hotmail.com>
2019-05-13 11:12:04 +0300

    Fix responsiveness

--- gallery/views.py
+++ gallery/views.py
@@ -4,11 +4,21 @@ from .models import Image, Location, Category, Photographer
 
 # Create your views here.
 def index(request):
+
+    '''
+    view function to display landing page
+    '''
+
     images = Image.objects.all()
 
     return render(request, 'index.html', {"images": images})
 
+
 def search_page(request):
+
+    '''
+    view function to open search page and display searched images
+    '''
 
     if 'search' in request.GET and request.GET["search"]:
         search_term = request.GET.get("search")
@@ -22,10 +32,20 @@ def search_page(request):
         return render(request, 'search.html',{"message":message})
 
 def sortby_locations(request):
+
+    '''
+    view function to display images sorted by Location
+    '''
+
     images = Image.filter_by_location()
 
     return render(request, 'location.html', {"images":images})
 
 def single_image(request, image_id):
+
+    '''
+    view function to display a single image and its details
+    '''
+
     image = Image.get_image_by_id(image_id)
     return render(request, 'single_image.html', {"image":image})
